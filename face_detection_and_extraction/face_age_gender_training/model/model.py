1dd2974d389ddb184e57f6058f5e19f2e882d4d3
SamSamhuns <samhunsadamant@gmail.com>
2021-11-17 13:39:36 +0400

    Add a fully connected NN arch

--- face_detection_and_extraction/face_age_gender_training/model/model.py
+++ face_detection_and_extraction/face_age_gender_training/model/model.py
@@ -131,3 +131,39 @@ class ResMLP(BaseModel):
 
     def forward(self, x: torch.tensor) -> torch.Tensor:
         return self.blocks(x)
+
+
+class FullyConnectedNet(BaseModel):
+    """
+    Classic fully connected neural network that downsamples features by half every layer
+    """
+
+    def __init__(self, num_blocks: int, num_classes: int,
+                 num_initial_features: int, reduce_in_features: int, **kwargs):
+        super().__init__()
+
+        blocks = []
+        # input feature space reduction layer, acts as encoder layer
+        # if reduce_feat_num is not None, reduce input features with downsampling instead of residual block
+        if reduce_in_features is not None:
+            blocks.append(nn.Linear(num_initial_features, reduce_in_features))
+            blocks.append(nn.ReLU())
+        else:
+            reduce_in_features = num_initial_features
+
+        for i in range(num_blocks):
+            blocks.extend(self._create_block(reduce_in_features))
+            reduce_in_features //= 2
+
+        # last classification layer
+        blocks.append(nn.Linear(reduce_in_features, num_classes))
+        self.blocks = nn.Sequential(*blocks)
+
+    def _create_block(self, in_features: int) -> list:
+        block = []
+        block.append(nn.Linear(in_features, in_features // 2))
+        block.append(nn.ReLU())
+        return block
+
+    def forward(self, x: torch.tensor) -> torch.Tensor:
+        return self.blocks(x)

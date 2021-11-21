4a3395228705604ec693e7c9214fe667016db336
SamSamhuns <samhunsadamant@gmail.com>
2021-11-21 10:12:02 +0400

    Drop last in video data ldr

--- face_detection_and_extraction/face_age_gender_training/base/base_data_loader.py
+++ face_detection_and_extraction/face_age_gender_training/base/base_data_loader.py
@@ -23,7 +23,8 @@ class BaseDataLoader(DataLoader):
             'batch_size': batch_size,
             'shuffle': self.shuffle,
             'collate_fn': collate_fn,
-            'num_workers': num_workers
+            'num_workers': num_workers,
+            'drop_last': True
         }
         super().__init__(sampler=self.sampler, **self.init_kwargs)
 
